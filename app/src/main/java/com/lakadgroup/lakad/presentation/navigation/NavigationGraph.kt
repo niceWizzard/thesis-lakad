5cf356df6c367454e75d19f01b997956883b947f
niceWizzard <richard232manansala@gmail.com>
2025-11-04 10:08:54 +0800

    - Moves the `contentWindowInsets` exclusion from the root `NavigationGraph`'s `Scaffold` to the `ExploreTab`'s `Scaffold`. - Removes the padding modifier from the `NavHost` in `NavigationGraph` as it is no longer needed. - Applies padding within the `ExploreTab` to correctly position content below the top app bar.

--- app/src/main/java/com/lakadgroup/lakad/presentation/navigation/NavigationGraph.kt
+++ app/src/main/java/com/lakadgroup/lakad/presentation/navigation/NavigationGraph.kt
@@ -1,11 +1,8 @@
 package com.lakadgroup.lakad.presentation.navigation
 
-import androidx.compose.foundation.layout.exclude
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,21 +13,17 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun NavigationGraph() {
     val navController = rememberNavController()
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        contentWindowInsets = ScaffoldDefaults.contentWindowInsets.exclude(
-            NavigationBarDefaults.windowInsets
-        ),
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            modifier = Modifier.padding(innerPadding),
             startDestination = NavRoutes.TabRoute,
         ) {
             composable<NavRoutes.TabRoute> {
                 TabNavigationGraph(rootNavController = navController)
             }
-
             composable<NavRoutes.OtherRoute> {
                 Text("Other Route")
             }
--- app/src/main/java/com/lakadgroup/lakad/presentation/tab/ExploreTab.kt
+++ app/src/main/java/com/lakadgroup/lakad/presentation/tab/ExploreTab.kt
@@ -1,16 +1,17 @@
 package com.lakadgroup.lakad.presentation.tab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.lakadgroup.lakad.presentation.navigation.NavRoutes
 import com.mapbox.maps.extension.compose.MapboxMap
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,10 +24,13 @@ fun ExploreTab(modifier: Modifier = Modifier) {
                     Text("Explore")
                 }
             )
-        }
-    ) {
+        },
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets.exclude(
+            NavigationBarDefaults.windowInsets,
+        ),
+    ) { innerPadding ->
         Column(
-            modifier = modifier.padding(it).fillMaxSize()
+            modifier = modifier.fillMaxSize().padding(innerPadding)
         ) {
             MapboxMap {
 
